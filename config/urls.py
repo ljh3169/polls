848d87bd8cf4ad4a90fab8d4a49fa564e70a0a37
ljh3169 <ittrb@naver.com>
2022-02-24 12:05:21 +0900

    설문조사 앱

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from poll import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),  #127.0.0.1:8000
-    path('poll/', include('poll.urls'))
+    path('', views.index),    #127.0.0.1:8000
+    path('poll/', include('poll.urls')),
 ]
--- poll/urls.py
+++ poll/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),   #127.0.0.1:8000/poll/
-    path('cart/', views.cart)          #127.0.0.1:8000/poll/cart/
+    path('', views.index),      #127.0.0.1:8000/poll/
+    path('cart/', views.cart),  #127.0.0.1:8000/poll/cart/
 ]
